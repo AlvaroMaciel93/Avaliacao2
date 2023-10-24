5b905df0a56f80ee72566aee4bfa256506e6ba23
Gabriel <gabrielsousa4242@gmail.com>
2023-10-24 19:13:13 -0300

    adicionando o arquivo da classe lista data

---
+++ listaDatas.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class ListaDatas  {
+	vector<Data> lista;
+	
+	public:
+		
+	/*
+	O m�todo abaixo pergunta ao usu�rios quantos
+	elementos v�o existir na lista e depois
+	solicita a digita��o de cada um deles
+	*/	
+	void entradaDeDados() {
+		
+	}
+	
+	void mostraMediana() {
+		cout << "Aqui vai mostrar a mediana da lista de datas" << endl;
+	}
+	
+	void mostraMenor() {
+		cout << "Aqui vai mostrar a primeira data cronologicamente" << endl;
+	}
+	void mostraMaior() {
+		cout << "aqui vai mostrar a ultima data cronologicamente" << endl;
+	}
+};
