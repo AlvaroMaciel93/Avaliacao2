a1e0b21cfdf1f9a497c0088ac73430e21c633796
Álvaro Maciel Cabral Dantas <142815803+AlvaroMaciel93@users.noreply.github.com>
2023-10-25 21:36:30 -0300

    Último commit da parte 1 - Remoção de elementos desnecessários e correções de exibição/impressão

--- listanomes.cpp
+++ listanomes.cpp
@@ -1,52 +1,44 @@
-class ListaNomes : public Lista<string>
-{
-public:
-	void entradaDeDados() override
-	{
-		int n;
-		cout << "Quantos nomes deseja inserir? ";
-		cin >> n;
-		cin.ignore();
-
-		for (int i = 0; i < n; i++)
-		{
-			string nome;
-            cout << "Nome " << i + 1 << ": ";
-            getline(cin, nome);
-			adicionar(nome);
+class ListaNomes : public Lista<string> {
+	public:
+		void entradaDeDados() override {
+			int n;
+			cout << "Quantos nomes deseja inserir? ";
+			cin >> n;
+			cin.ignore();
+			for (int i = 0; i < n; i++) {
+				string nome;
+            			cout << "Nome " << i + 1 << ": ";
+            			getline(cin, nome);
+				adicionar(nome);
+			}
 		}
-	}
 
-	void mostraMediana() override {
-        	if (elementos.empty()) {
-            		cout << "A lista de nomes está vazia." << endl;
-            	return;
-        	}
-        	cout << "Calculando a mediana dos nomes..." << endl;
-        	sort(elementos.begin(), elementos.end()); // Ordena a lista
+		void mostraMediana() override {
+        		if (elementos.empty()) {
+            			cout << "A lista de nomes está vazia." << endl;
+            		return;
+        		}
+        		sort(elementos.begin(), elementos.end()); // Ordena a lista
+			int tamanho = elementos.size();
+        		int meio1 = tamanho / 2 - 1;
 
-        	int tamanho = elementos.size();
-        	int meio1 = tamanho / 2 - 1;
-
-        	// Se a quantidade de elementos é par, escolha o primeiro deles
-        	if (tamanho % 2 == 0) {
-            		cout << "Mediana: " << elementos[meio1] << endl;
-        	}
-		else {
-            		cout << "Mediana: " << elementos[meio1 + 1] << endl;
-        	}
-    	}
-	void mostraMenor() override {
-		cout << "Mostrando o primeiro nome alfabeticamente..." << endl;
-		if (!this->elementos.empty()) {
-			cout << "Menor nome: " << *min_element(this->elementos.begin(), this->elementos.end()) << endl;
+        		// Se a quantidade de elementos é par, escolha o primeiro deles
+        		if (tamanho % 2 == 0) {
+            			cout << "Mediana dos nomes: " << elementos[meio1] << endl;
+        		}
+			else {
+            			cout << "Mediana dos nomes: " << elementos[meio1 + 1] << endl;
+        		}
+    		}
+		void mostraMenor() override {
+			if (!this->elementos.empty()) {
+				cout << "Menor nome: " << *min_element(this->elementos.begin(), this->elementos.end()) << endl;
+			}
 		}
-	}
 
-	void mostraMaior() override {
-		cout << "Mostrando o último nome alfabeticamente..." << endl;
-		if (!this->elementos.empty()) {
-			cout << "Maior nome: " << *max_element(this->elementos.begin(), this->elementos.end()) << endl;
+		void mostraMaior() override {
+			if (!this->elementos.empty()) {
+				cout << "Maior nome: " << *max_element(this->elementos.begin(), this->elementos.end()) << endl;
+			}
 		}
-	}
 };
